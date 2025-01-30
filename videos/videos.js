622a7f4bf2c61c13870d7d8b4b472cad454543b7
Arqueid <79342761+Arqueid@users.noreply.github.com>
2025-01-30 15:03:55 -0300

    nova pasta de projetos

---
+++ videos/videos.js
@@ -0,0 +1,25 @@
+// videos.js
+const videos = [
+    {
+        id: 997,
+        title: "Mulata Dançando Pelada",
+        embedUrl: "https://www.xvideos.com/embedframe/kedeffb5f2c",
+        coverImage: "https://cdn77-pic.xvideos-cdn.com/videos/thumbs169poster/91/86/35/9186357e647630de269dd7e8989622ac-2/9186357e647630de269dd7e8989622ac.11.jpg",
+        tags: ["mulata", "pelada", "dançando pelada", "dançando"]
+    },
+    {
+        id: 998,
+        title: "gostosas na academia",
+        embedUrl: "https://www.xvideos.com/embedframe/kedavfb23f9",
+        coverImage: "https://cdn77-pic.xvideos-cdn.com/videos/thumbs169poster/ec/b9/c1/ecb9c16d140c1fc47719d50655eb66ff/ecb9c16d140c1fc47719d50655eb66ff.17.jpg",
+        tags: ["academia", "dança", "dançando", "musica", "calcinha"]
+    },
+    {
+        id: 999,
+        title: "Gostosas Zumba",
+        embedUrl: "https://www.xvideos.com/embedframe/kedvfuo9770",
+        coverImage: "https://gcore-pic.xvideos-cdn.com/videos/thumbs169poster/b7/cf/89/b7cf8909b1be36d90b223ac4a83ed4ea/b7cf8909b1be36d90b223ac4a83ed4ea.2.jpg",
+        tags: ["dançando", "dança", "jeans", "calca", "calça", "musica", "zumba"]
+    }
+    // Adicione mais vídeos conforme necessário
+];
