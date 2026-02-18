0f8f35fc8eec1903755675076575c458b83e9587
Arqueid <79342761+Arqueid@users.noreply.github.com>
2026-02-18 07:15:03 -0300

    nova extrutura
    novos arquivos

---
+++ dados.js
@@ -0,0 +1,43 @@
+// BANCO DE DADOS MANUAL
+// Para adicionar mais notícias, copie um bloco entre chaves {} e cole abaixo, separando por vírgula.
+
+// os 4 Primeiros tem destaque principalmente o 1
+// digite final para ir ao fm das IDs
+const bancoDeDados = [
+    {
+        id: 1,
+        titulo: "Lançamento do novo Portal Dark Mode agita a web",
+        resumo: "Desenvolvedores elogiam a interface limpa e a velocidade de carregamento do novo layout inspirado em grandes portais.",
+        categoria: "TECNOLOGIA",
+        tempo: "há 2 horas",
+        imagem: "https://via.placeholder.com/320x180/b71c1c/ffffff?text=FOTO+16:9", // Link da imagem 16:9
+        link: "https://www.google.com" // Link para onde vai ao clicar
+    },
+    {
+        id: 2,
+        titulo: "Tutorial: Como criar menus responsivos com CSS",
+        resumo: "Aprenda passo a passo como fazer o menu hambúrguer funcionar sem usar frameworks pesados.",
+        categoria: "PROGRAMAÇÃO",
+        tempo: "há 5 horas",
+        imagem: "foto.jpg",
+        link: "https://www.w3schools.com"
+    },
+    {
+        id: 3,
+        titulo: "Tutorial: Como criar menus responsivos com CSS",
+        resumo: "Aprenda passo a passo como fazer o menu hambúrguer funcionar sem usar frameworks pesados.",
+        categoria: "PROGRAMAÇÃO",
+        tempo: "há 5 horas",
+        imagem: "foto.jpg",
+        link: "https://www.w3schools.com"
+    },
+    {
+        id: 4,
+        titulo: "O futuro do Javascript no desenvolvimento Front-end",
+        resumo: "Especialistas discutem as novas atualizações da linguagem e o que esperar para 2026.",
+        categoria: "MERCADO",
+        tempo: "há 1 dia",
+        imagem: "https://via.placeholder.com/320x180/333333/ffffff?text=JS+FUTURO",
+        link: "https://developer.mozilla.org"
+    }
+];
